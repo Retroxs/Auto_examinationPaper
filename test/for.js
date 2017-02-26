3b8360f2cb4df422c4fdfc0792ad0879e666d5fa
retroxs <jssch@foxmail.com>
2017-02-26 17:04:54 +0800

    merge dev

---
+++ test/for.js
@@ -0,0 +1,11 @@
+/**
+ * Created by HUI on 2017/2/26.
+ */
+(function () {
+    for(var a =10;a<15;a++){
+
+        for(var b=0;b<5;b++){
+            console.log('a='+a+'  '+'b='+b)
+        }
+    }
+})()
